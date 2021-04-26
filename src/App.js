da8da5ea7dab80f618bfe3e3639682a1b215fd02
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-26 16:43:19 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Card, Col, Container, Row, Tab, Tabs} from "react-bootstrap";
+import {Button, Card, Col, Container, Modal, Row, Tab, Tabs} from "react-bootstrap";
 
 function App() {
   return (
@@ -7,8 +7,7 @@ function App() {
       <Row className="recipe-review-overall-rating justify-content-between mb-3">
         <Col xs="auto">
           <h2 className="section-headline">
-            <a id="reviewSection"></a>
-            <span className="review-headline">Reviews</span>
+            <span className="review-headline">Reviews</span>{' '}
             <span className="review-headline-count">(15)</span>
           </h2>
         </Col>
@@ -1822,6 +1821,127 @@ function App() {
           </Tabs>
         </Col>
       </Row>
+
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>
+            <div className="ugc-upload-banner">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 20" role="img"
+                   aria-hidden="true" tabIndex="-1"><title>Rate Review</title>
+                <path
+                  d="M12.726 10.694L9.998 9.267l-2.719 1.43.52-3.025-2.205-2.15 3.04-.446L9.998 2.32l1.363 2.756 3.042.44-2.196 2.15.52 3.028zM18.236 0H1.761C.001 0 .001 1.606.001 1.606V12.16s-.057 1.587 1.524 1.587h1.638v5.476l5.475-5.476h9.836c1.579 0 1.526-1.587 1.526-1.587V1.606S20 0 18.236 0z"></path>
+              </svg>
+              Review this recipe
+            </div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col>Chunky Italian Spaghetti Sauce</Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="component recipe-ratings interactive">
+                <fieldset className="ratings-text ratings-input">
+                  <legend className="ratings-legend">Rate this recipe</legend>
+                  <input name="ugc-rating" type="radio" value="" id="star0"
+                         className="rating-star-input visually-hidden star0" checked=""/>
+                  <label htmlFor="star0" className="rating-star rating-star-label">
+                    <span className="ratings-text-label" data-rating="0">Your rating</span>
+                  </label>
+                  <input name="ugc-rating" type="radio" value="1" id="star1"
+                         className="rating-star-input visually-hidden star1"/>
+                  <label htmlFor="star1" className="rating-star rating-star-label">
+                    <span className="visually-hidden">Rate this a 1: </span>
+                    <svg width="44" height="44" viewBox="0 0 24 24" role="img" aria-hidden="true" tabIndex="-1"
+                         xmlns="http://www.w3.org/2000/svg">
+                      <path className="rating-star-filled"
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                    </svg>
+                    <span className="ratings-text-label" data-rating="1">Couldn't eat it</span>
+                  </label>
+                  <input name="ugc-rating" type="radio" value="2" id="star2"
+                         className="rating-star-input visually-hidden star2"/>
+                  <label htmlFor="star2" className="rating-star rating-star-label">
+                    <span className="visually-hidden">Rate this a 2: </span>
+                    <svg width="44" height="44" viewBox="0 0 24 24" role="img" aria-hidden="true" tabIndex="-1"
+                         xmlns="http://www.w3.org/2000/svg">
+                      <path className="rating-star-filled"
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                    </svg>
+                    <span className="ratings-text-label" data-rating="2">Didn't like it</span>
+                  </label>
+                  <input name="ugc-rating" type="radio" value="3" id="star3"
+                         className="rating-star-input visually-hidden star3"/>
+                  <label htmlFor="star3" className="rating-star rating-star-label">
+                    <span className="visually-hidden">Rate this a 3: </span>
+                    <svg width="44" height="44" viewBox="0 0 24 24" role="img" aria-hidden="true" tabIndex="-1"
+                         xmlns="http://www.w3.org/2000/svg">
+                      <path className="rating-star-filled"
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                    </svg>
+                    <span className="ratings-text-label" data-rating="3">It was OK</span>
+                  </label>
+                  <input name="ugc-rating" type="radio" value="4" id="star4"
+                         className="rating-star-input visually-hidden star4"/>
+                  <label htmlFor="star4" className="rating-star rating-star-label">
+                    <span className="visually-hidden">Rate this a 4: </span>
+                    <svg width="44" height="44" viewBox="0 0 24 24" role="img" aria-hidden="true"
+                         tabIndex="-1" xmlns="http://www.w3.org/2000/svg">
+                      <path className="rating-star-filled"
+                            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                    </svg>
+                    <span className="ratings-text-label" data-rating="4">Liked it</span>
+                  </label>
+                  <input name="ugc-rating" type="radio" value="5" id="star5"
+                         className="rating-star-input visually-hidden star5"/>
+                  <label htmlFor="star5" className="rating-star rating-star-label">
+                              <span className="visually-hidden">Rate this a 5: </span>
+                              <svg width="44" height="44" viewBox="0 0 24 24" role="img" aria-hidden="true"
+                                   tabIndex="-1" xmlns="http://www.w3.org/2000/svg">
+                                <path className="rating-star-filled"
+                                      d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                              </svg>
+                              <span className="ratings-text-label" data-rating="5">Loved it</span>
+                            </label>
+                </fieldset>
+              </div>
+              <div className="ugc-upload-text">
+                <label className="visually-hidden" htmlFor="ugc-text">What did you think about this recipe? Did you make
+                  any changes or notes?</label>
+                <textarea id="ugc-text" name="ugc-text"
+                          placeholder="What did you think about this recipe? Did you make any changes or notes? "></textarea>
+              </div>
+            </Col>
+            <Col>
+              <div className="component ugc-photo-upload">
+                <input className="ugc-photo-upload__uniqueId js--uploadedPhotoId" type="hidden"/>
+                  <div className="ugc-photo-upload__help">
+                        <span className="ugc-photo-upload__helpIcon">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" role="img"
+                               aria-hidden="true" tabIndex="-1"><path
+                            d="M3 4V1h2v3h3v2H5v3H3V6H0V4h3zm3 6V7h3V4h7l1.83 2H21c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V10h3zm7 9c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-3.2-5c0 1.77 1.43 3.2 3.2 3.2 1.77 0 3.2-1.43 3.2-3.2 0-1.77-1.43-3.2-3.2-3.2-1.77 0-3.2 1.43-3.2 3.2z"></path></svg>
+                        </span>
+                    <label htmlFor="photoUploadFile" className="ugc-photo-upload__helpText">Add Photo</label>
+                  </div>
+                  <div className="ugc-photo-upload__uploadedImage js--uploadedImage">
+                        <span className="ugc-photo-upload__editPhotoIcon">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16" role="img"
+                               aria-hidden="true"><path
+                            d="M0 16l5.08-2.031 7.619-7.62-3.811-3.808-7.619 7.618L0 16zM15.05 3.098L12.139.185a.635.635 0 0 0-.897 0L10.156 1.27l3.81 3.809 1.085-1.083a.637.637 0 0 0 0-.898z"></path></svg>
+                        </span>
+                  </div>
+                  <input type="file" id="photoUploadFile" className="ugc-photo-upload__uploadInput js--fileInput"
+                         data-photo-max-upload-size="2097152" accept="image/*"/>
+              </div>
+            </Col>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </Container>
   );
 }
